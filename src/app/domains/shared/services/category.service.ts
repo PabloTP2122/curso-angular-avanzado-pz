0e5f9c4f713367bde61bd57bd4b663854fe887a5
PabloTP <pablo_tp@exatec.tec.mx>
2025-06-24 16:34:50 -0600

    Add: add pred/dev environments

--- src/app/domains/shared/services/category.service.ts
+++ src/app/domains/shared/services/category.service.ts
@@ -1,16 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Category } from '@shared/models/category.model';
+import { environment } from '@env/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CategoryService {
   private http = inject(HttpClient);
+  private apiUrl = environment.apiUrl;
 
   getAll() {
-    return this.http.get<Category[]>(
-      `https://api.escuelajs.co/api/v1/categories`,
-    );
+    return this.http.get<Category[]>(`${this.apiUrl}/api/v1/categories`);
   }
 }
