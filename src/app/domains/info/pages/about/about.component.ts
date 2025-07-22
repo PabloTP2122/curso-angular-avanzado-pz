04abe9433ab02737f9903fb62e28e036f49d3520
PabloTP <pablo_tp@exatec.tec.mx>
2025-07-22 15:02:01 -0600

    Feature(about): interoperability with observables using toSignal

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { toSignal } from '@angular/core/rxjs-interop';
+
 import { CounterComponent } from '@shared/components/counter/counter.component';
 import { HighlightDirective } from '@shared/directives/highlight.directive';
 
 import { WaveAudioComponent } from '@info/components/wave-audio/wave-audio.component';
 import { FormsModule } from '@angular/forms';
+import { BehaviorSubject, delay, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-about',
@@ -22,6 +25,18 @@ export default class AboutComponent {
   duration = signal(1000);
   message = signal('Hola');
 
+  obsWithInit$ = new BehaviorSubject<string>('init value');
+  $withInit = toSignal(this.obsWithInit$, {
+    requireSync: true,
+  });
+
+  obsWithOutInit$ = new Subject<string>();
+  //this.obsWithOutInit$.pipe(delay(1000))
+  //Se usa por si el valor por defecto tiene que esperarse, por ejemplo desde API
+  $withOutInit = toSignal(this.obsWithOutInit$.pipe(delay(1000)), {
+    initialValue: '----',
+  });
+
   changeDuration(event: Event) {
     const input = event.target as HTMLInputElement;
     this.duration.set(input.valueAsNumber);
@@ -30,5 +45,13 @@ export default class AboutComponent {
   changeMessage(event: Event) {
     const input = event.target as HTMLInputElement;
     this.message.set(input.value);
+  }
+
+  emitWithInit() {
+    this.obsWithInit$.next('New value');
+  }
+
+  emitWithOutInit() {
+    this.obsWithOutInit$.next('++++');
   }
 }
