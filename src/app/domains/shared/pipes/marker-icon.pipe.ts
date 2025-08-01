fa8b9cf03627281a9e5a548deb340b01dd1b2b66
PabloTP <pablo_tp@exatec.tec.mx>
2025-08-01 14:53:28 -0600

    feat(marker-icon): actualizar color de ícono en el pipe de marcadores

--- src/app/domains/shared/pipes/marker-icon.pipe.ts
+++ src/app/domains/shared/pipes/marker-icon.pipe.ts
@@ -10,9 +10,9 @@ export class MarkerIconPipe implements PipeTransform {
   private iconCache = new Map<number, HTMLElement>();
 
   private readonly iconContent = `
-      <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="#ff2825" stroke-linecap="round" stroke-linejoin="round">
+      <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="#0AE98A" stroke-linecap="round" stroke-linejoin="round">
         <path d="M18.364 4.636a9 9 0 0 1 .203 12.519l-.203 .21l-4.243 4.242a3 3 0 0 1 -4.097 .135l-.144 -.135l-4.244 -4.243a9 9 0 0 1 12.728 -12.728z"/>
-        <path d="M12 9a3 3 0 1 0 0 6a3 3 0 0 0 0 -6z" fill="#fff" />
+        <path d="M12 9a3 3 0 1 0 0 6a3 3 0 0 0 0 -6z" fill="#000" />
       </svg>
     `;
   transform(location: Location): HTMLElement {
