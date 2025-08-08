dec48ebe4185a425af87d37afb744449c5e8403a
PabloTP <pablo_tp@exatec.tec.mx>
2025-08-08 17:17:31 -0600

    feat(product-detail): actualizar la obtención de datos del producto y mejorar la gestión de meta tags

--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -1,26 +1,26 @@
-import {
-  Component,
-  inject,
-  signal,
-  OnInit,
-  input,
-  linkedSignal,
-} from '@angular/core';
+import { Component, effect, inject, input, linkedSignal } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { ProductService } from '@shared/services/product.service';
 import { Product } from '@shared/models/product.model';
 import { CartService } from '@shared/services/cart.service';
+import { Meta, Title } from '@angular/platform-browser';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { environment } from '@env/environment';
 
 @Component({
   selector: 'app-product-detail',
   imports: [CommonModule, NgOptimizedImage],
   templateUrl: './product-detail.component.html',
 })
-export default class ProductDetailComponent implements OnInit {
-  readonly slug = input<string>();
-  product = signal<Product | null>(null);
+export default class ProductDetailComponent {
+  readonly slug = input.required<string>();
+  productRs = rxResource({
+    params: () => ({ slug: this.slug() }),
+    stream: ({ params }) => this.productService.getBySlug(params.slug),
+  });
+
   $cover = linkedSignal({
-    source: this.product,
+    source: this.productRs.value,
     computation: (product, previousValue) => {
       return product && product?.images.length > 0
         ? product.images[0]
@@ -30,18 +30,33 @@ export default class ProductDetailComponent implements OnInit {
   private productService = inject(ProductService);
   private cartService = inject(CartService);
 
-  ngOnInit() {
-    const slug = this.slug();
-    if (slug) {
-      this.productService.getBySlug(slug).subscribe({
-        next: product => {
-          this.product.set(product);
-          if (product.images.length > 0) {
-            this.$cover.set(product.images[0]);
-          }
-        },
-      });
-    }
+  //Meta tags dinámicas
+  titleService = inject(Title);
+  metaService = inject(Meta);
+
+  constructor() {
+    effect(() => {
+      const product = this.productRs.value();
+      if (product) {
+        this.titleService.setTitle(product.title);
+        this.metaService.updateTag({
+          name: 'og:title',
+          content: product.title,
+        });
+        this.metaService.updateTag({
+          name: 'og:image',
+          content: product.images[0],
+        });
+        this.metaService.updateTag({
+          name: 'og:description',
+          content: product.description,
+        });
+        this.metaService.updateTag({
+          name: 'og:url',
+          content: `${environment.domain}/product/${product.slug}`,
+        });
+      }
+    });
   }
 
   changeCover(newImg: string) {
@@ -49,7 +64,7 @@ export default class ProductDetailComponent implements OnInit {
   }
 
   addToCart() {
-    const product = this.product();
+    const product = this.productRs.value() as Product | undefined;
     if (product) {
       this.cartService.addToCart(product);
     }
