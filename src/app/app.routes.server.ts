408e53650fcf751cb1c1cb214b927abb1b068d30
PabloTP <pablo_tp@exatec.tec.mx>
2025-08-08 15:26:12 -0600

    feat(routes.server): agregar rutas para 'about', 'category', 'product' y 'locations' con modos de renderizado correspondientes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -5,4 +5,20 @@ export const serverRoutes: ServerRoute[] = [
     path: '**',
     renderMode: RenderMode.Prerender,
   },
+  {
+    path: 'about', // This page is static, so we prerender it (SSG)
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'category/:slug',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'product/:slug',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'locations',
+    renderMode: RenderMode.Client,
+  },
 ];
